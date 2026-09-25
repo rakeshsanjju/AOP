8206f032d6035c76404c42a4cbc06595d6e54f48
unknown <sanjurakeshdasari4@gmail.com>
2026-09-25 12:28:08 +0530

    Real Domain Added

--- back-end/server.js
+++ back-end/server.js
@@ -21,7 +21,7 @@ const allowedOrigins = new Set([
     "http://127.0.0.1:5500",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://aop.avisvascularcentre.com/"
+    "https://aop.avisvascularcentre.com"
 ]);
 
 const corsOptions = {
