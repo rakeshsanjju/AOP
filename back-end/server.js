68218df3a55c09cd0a6a5d4ce3adf818de359d11
unknown <sanjurakeshdasari4@gmail.com>
2026-09-25 11:49:45 +0530

    HR MAILS ADDED

--- back-end/server.js
+++ back-end/server.js
@@ -20,7 +20,8 @@ const allowedOrigins = new Set([
     "http://localhost:5500",
     "http://127.0.0.1:5500",
     "http://localhost:3000",
-    "http://127.0.0.1:3000"
+    "http://127.0.0.1:3000",
+    "https://aop.avisvascularcentre.com/"
 ]);
 
 const corsOptions = {
